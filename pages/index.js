8963409298bff5f91a1de91c35a9b162b395adde
Chau Giang <admin@devops.ci>
2023-03-26 16:36:55 +0700

    blog: update code to using card item and card list item component

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import { Col, Container, Card, Image, NavDropdown, Row } from 'react-bootstrap';
 
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro'
+import CardListItem from 'components/CardListItem';
+import CardItem from 'components/CardItem';
 
 export default function Home() {
   return (
@@ -16,65 +18,11 @@ export default function Home() {
       <hr/>
       <Row className="mb-5">
         <Col md="10">
-          {/* CardListItem STARTS */}
-          <Card className={`fj-card fj-card-list`}>
-            <div className="card-body-wrapper">
-              <Card.Header
-                className="d-flex flex-row">
-                <img
-                  src={'https://via.placeholder.com/150'}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"/>
-                  <div>
-                    <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
-                    <Card.Text className="card-date">Placeholder Date</Card.Text>
-                  </div>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title className="card-main-title">Placeholder Title</Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a href="#" className="card-button">
-              Read More
-            </a>
-          </Card>
-          {/* CardListItem ENDS */}
+          <CardListItem />
         </Col>
 
         <Col md="4">
-          <Card className={`fj-card`}>
-            <div className="card-body-wrapper">
-              <Card.Header
-                className="d-flex flex-row">
-                <img
-                  src={'https://via.placeholder.com/150'}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"/>
-                <div>
-                  <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
-                  <Card.Text className="card-date">Placeholder Date</Card.Text>
-                </div>
-              </Card.Header>
-              <div className="view overlay">
-                <Card.Img
-                  src='https://via.placeholder.com/250'
-                  alt="Card image cap"
-                />
-              </div>
-              <Card.Body>
-                <Card.Title className="card-main-title">Placeholder Title</Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a className="card-button">
-              Read More
-            </a>
-          </Card>
+          <CardItem />
         </Col>
       </Row>
     </PageLayout>
