94bf81b113ebacbd60bcd9720ec99db4b644dd06
Chau Giang <admin@devops.ci>
2023-03-29 16:05:39 +0700

    blog: get static props

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 import { Col, Container, Card, Image, NavDropdown, Row } from 'react-bootstrap';
+
+import {getAllBlogs} from 'lib/api'
 
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro'
 import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <PageLayout>
       <AuthorIntro/>
       <hr/>
+      {JSON.stringify(props.blogs)}
       <Row className="mb-5">
         <Col md="10">
           <CardListItem />
         </Col>
-
         <Col md="4">
           <CardItem />
         </Col>
       </Row>
     </PageLayout>
   )
+}
+
+export async function getStaticProps() {
+  const blogs = await getAllBlogs()
+  return {
+    props: {
+     blogs,
+    }
+  }
 }
