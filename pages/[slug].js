4dbc91f71d474c63afd0f6b9dbaede94e71ca687
Chau Giang <admin@devops.ci>
2023-03-30 00:29:40 +0700

    blog: get static post by slug

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,14 +1,30 @@
-import {useRouter} from 'next/router'
+import PageLayout from 'components/PageLayout'
 
-import PageLayout from "components/PageLayout"
+import { getAllPosts, getPostBySlug } from 'lib/api'
 
-const PostDetail = () => {
-  const {query: {slug}} = useRouter()
+const PostDetail = ({post}) => {
   return (
     <PageLayout>
-      <h1>Hello {slug}</h1>
+      <h1>Hello {post.slug}</h1>
     </PageLayout>
   )
 }
 
+export async function getStaticProps({params: {slug}}) {
+  const post = await getPostBySlug(slug)
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = await getAllPosts()
+  const paths = posts.map(({slug}) => ({ params: { slug }}))
+  return  {
+    paths,
+    fallback: false,
+  }
+}
 export default PostDetail
