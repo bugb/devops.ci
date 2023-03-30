1614e0634ec7b37c0b4210f0b32c38296bfc80c0
Chau Giang <admin@devops.ci>
2023-03-30 17:27:09 +0700

    styles: change font to albert sans

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,11 @@ import '@/styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/index.scss';
 
-
+const albertSans = Albert_Sans({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <main className = {albertSans.className}>
+      <Component {...pageProps} />
+    </main>
   )
 }
