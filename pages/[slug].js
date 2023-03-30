cce0c2cfd77fcd5db7dd9095041b90cdce567413
Chau Giang <admin@devops.ci>
2023-03-30 23:36:27 +0700

    blog: display posts detail content (include image and code)

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,8 +3,9 @@ import { Row, Col } from 'react-bootstrap'
 import PageLayout from 'components/PageLayout'
 import { getAllPosts, getPostBySlug } from 'lib/api'
 import BlogHeader from 'components/BlogHeader'
+import BlogContent from 'components/BlogContent'
 
-const PostDetail = ({post: {title, subtitle, coverImage, _updatedAt, author: {name, avatar}}}) => {
+const PostDetail = ({post: {title, subtitle, coverImage, _updatedAt, author: {name, avatar}, content}}) => {
   return (
     <PageLayout className="blog-detail-page">
       <Row>
@@ -17,9 +18,9 @@ const PostDetail = ({post: {title, subtitle, coverImage, _updatedAt, author: {na
             avatar={avatar}
             updatedAt={_updatedAt}
           />
-          <hr/>
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          <BlogContent content={content} />
         </Col>
+        <hr/>
       </Row>
     </PageLayout>
   )
